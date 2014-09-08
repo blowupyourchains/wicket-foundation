679436cdb78d75eb945e57cbe8098ceeae9ff668
Ilkka Seppala <iluwatar@gmail.com>
2014-09-08 18:51:55 +0300

    Added dropdown sample page. Added link to catalog page.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/DropdownsPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class DropdownsPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public DropdownsPage(PageParameters params) {
+		super(params);
+	}
+}
