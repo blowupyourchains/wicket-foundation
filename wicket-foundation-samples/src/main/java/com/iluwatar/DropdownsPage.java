5a400c4740e5310b4ee102b1bb7e59877a2b7756
Ilkka Seppala <iluwatar@gmail.com>
2014-09-13 11:50:27 +0300

    Content / dropdowns sample / advanced example.

--- wicket-foundation-samples/src/main/java/com/iluwatar/DropdownsPage.java
+++ wicket-foundation-samples/src/main/java/com/iluwatar/DropdownsPage.java
@@ -8,6 +8,7 @@ import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.iluwatar.foundation.dropdown.DropdownOptions;
+import com.iluwatar.foundation.dropdown.DropdownType;
 import com.iluwatar.foundation.dropdown.FoundationContentDropdown;
 import com.iluwatar.foundation.dropdown.FoundationDropdown;
 
@@ -34,5 +35,18 @@ public class DropdownsPage extends BasePage {
 		final String content = "<p>Some text that people will think is awesome! Some text that people will think is awesome! Some text that people will think is awesome!</p>";
 		FoundationContentDropdown basicContentDropdown = new FoundationContentDropdown("basicContentDropdown", "Has Content Dropdown", new DropdownOptions(), content);
 		add(basicContentDropdown);
+		
+		List<String> advancedLinks = Arrays.asList("This is a link", "This is another", "Yet another");
+		FoundationDropdown advancedDropdownLink = new FoundationDropdown("advancedDropdownLink", "Link Dropdown", new DropdownOptions(DropdownType.DROPDOWNLINK), advancedLinks) {
+				@Override
+				protected WebMarkupContainer createDropdownLink(int idx,
+						String id) {
+					return new Link<String>(id) {
+						@Override
+						public void onClick() {}
+					};
+				}
+		};
+		add(advancedDropdownLink);		
 	}
 }
