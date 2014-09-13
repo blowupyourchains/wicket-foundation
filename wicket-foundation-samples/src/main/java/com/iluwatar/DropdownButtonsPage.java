e5bcedc28f2ca60b7924f760aed15b2b0d1edf4f
Ilkka Seppala <iluwatar@gmail.com>
2014-09-13 17:23:40 +0300

    Added advanced example to dropdown buttons sample page.

--- wicket-foundation-samples/src/main/java/com/iluwatar/DropdownButtonsPage.java
+++ wicket-foundation-samples/src/main/java/com/iluwatar/DropdownButtonsPage.java
@@ -8,6 +8,9 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import com.iluwatar.foundation.button.ButtonColor;
+import com.iluwatar.foundation.button.ButtonRadius;
+import com.iluwatar.foundation.button.ButtonSize;
 import com.iluwatar.foundation.dropdown.DropdownOptions;
 import com.iluwatar.foundation.dropdown.DropdownType;
 import com.iluwatar.foundation.dropdown.FoundationDropdown;
@@ -21,6 +24,9 @@ public class DropdownButtonsPage extends BasePage {
 		
 		List<String> links = Arrays.asList("This is a link", "This is another", "Yet another");
 		add(createDropdownButton("basic", "Dropdown Button", links, new DropdownOptions(DropdownType.DROPDOWNBUTTON)));
+		List<String> advancedLinks = Arrays.asList("This is a link", "This is another", "Yet another");
+		DropdownOptions options = new DropdownOptions(DropdownType.DROPDOWNBUTTON).setSize(ButtonSize.LARGE).setRadius(ButtonRadius.ROUND).setColor(ButtonColor.ALERT);
+		add(createDropdownButton("advanced", "Dropdown Button", advancedLinks, options));
 	}
 	
 	private FoundationDropdown createDropdownButton(String id, String title, List<String> links, DropdownOptions options) {
