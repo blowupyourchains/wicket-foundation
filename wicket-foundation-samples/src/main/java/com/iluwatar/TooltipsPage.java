54b612b907ef92fbcd52b6fc02844612e6d10f5b
Ilkka Seppala <iluwatar@gmail.com>
2014-09-20 15:49:29 +0300

    Added tooltips sample page and rendering test.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/TooltipsPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class TooltipsPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public TooltipsPage(PageParameters params) {
+		super(params);
+	}
+}
