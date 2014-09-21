6232641d02bcf8369383c3e0acc9a3a533a22088
Ilkka Seppala <iluwatar@gmail.com>
2014-09-21 13:45:11 +0300

    Added top bar sample page skeleton and rendering test.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/TopBarPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class TopBarPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public TopBarPage(PageParameters params) {
+		super(params);
+	}
+}
