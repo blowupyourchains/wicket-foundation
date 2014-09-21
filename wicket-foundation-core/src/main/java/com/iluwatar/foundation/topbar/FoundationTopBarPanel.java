4247fbdb2b572d3aabe93170fd79fbd3fb0a8920
Ilkka Seppala <iluwatar@gmail.com>
2014-09-21 22:30:13 +0300

    Work on top bar component.

--- wicket-foundation-core/src/main/java/com/iluwatar/foundation/topbar/FoundationTopBarPanel.java
+++ wicket-foundation-core/src/main/java/com/iluwatar/foundation/topbar/FoundationTopBarPanel.java
@@ -2,37 +2,75 @@ package com.iluwatar.foundation.topbar;
 
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 import com.iluwatar.foundation.component.FoundationJsPanel;
 import com.iluwatar.foundation.util.Attribute;
+import com.iluwatar.foundation.util.StringUtil;
 
 public class FoundationTopBarPanel extends FoundationJsPanel {
 
 	private static final long serialVersionUID = 1L;
 
 	public FoundationTopBarPanel(String id) {
+		this(id, new TopBarOptions());
+	}
+	
+	public FoundationTopBarPanel(String id, TopBarOptions options) {
+		this(id, Model.of(options));
+	}
+	
+	public FoundationTopBarPanel(String id, IModel<TopBarOptions> optionsModel) {
 		super(id);
-		FoundationTopBarContainer topBarContainer = new FoundationTopBarContainer("topBarContainer");
+		FoundationTopBarContainer topBarContainer = new FoundationTopBarContainer("topBarContainer", optionsModel);
 		add(topBarContainer);
-		FoundationTopBar topBar = new FoundationTopBar("topBar");
+		FoundationTopBar topBar = new FoundationTopBar("topBar", optionsModel);
 		topBarContainer.add(topBar);
 	}
 	
 	private static class FoundationTopBarContainer extends WebMarkupContainer {
 
 		private static final long serialVersionUID = 1L;
+		
+		private IModel<TopBarOptions> optionsModel;
 
-		public FoundationTopBarContainer(String id) {
+		public FoundationTopBarContainer(String id, IModel<TopBarOptions> optionsModel) {
 			super(id);
+			this.optionsModel = optionsModel;
+		}
+		
+		@Override
+		protected void onComponentTag(ComponentTag tag) {
+			TopBarOptions options = optionsModel.getObject();
+			if (options.isFixed()) {
+				Attribute.addClass(tag, "fixed");
+			}
+			if (options.isContainToGrid()) {
+				Attribute.addClass(tag, "contain-to-grid");
+			}
+			if (options.isSticky()) {
+				Attribute.addClass(tag, "sticky");
+			}
+			super.onComponentTag(tag);
+		}
+		
+		@Override
+		protected void onDetach() {
+			optionsModel.detach();
+			super.onDetach();
 		}
 	}
 	
 	private static class FoundationTopBar extends WebMarkupContainer {
 
 		private static final long serialVersionUID = 1L;
+		
+		private IModel<TopBarOptions> optionsModel;
 
-		public FoundationTopBar(String id) {
+		public FoundationTopBar(String id, IModel<TopBarOptions> optionsModel) {
 			super(id);
+			this.optionsModel = optionsModel;
 		}
 		
 		@Override
@@ -40,7 +78,22 @@ public class FoundationTopBarPanel extends FoundationJsPanel {
 			Attribute.addClass(tag, "top-bar");
 			Attribute.addAttribute(tag, "data-topbar");
 			Attribute.addAttribute(tag, "role", "navigation");
+			TopBarOptions options = optionsModel.getObject();
+			if (options.isClickable()) {
+				Attribute.addDataOptions(tag, "is_hover:false");
+			}
+			if (!options.getStickySizes().isEmpty()) {
+				for (TopBarStickySize size : options.getStickySizes()) {
+					Attribute.addDataOptions(tag, "sticky_on:" + StringUtil.EnumNameToCssClassName(size.name()));
+				}
+			}
 			super.onComponentTag(tag);
+		}
+		
+		@Override
+		protected void onDetach() {
+			optionsModel.detach();
+			super.onDetach();
 		}
 	}
 }
--- wicket-foundation-core/src/test/java/com/iluwatar/foundation/topbar/FoundationTopBarPanelTest.java
+++ wicket-foundation-core/src/test/java/com/iluwatar/foundation/topbar/FoundationTopBarPanelTest.java
@@ -3,6 +3,8 @@ package com.iluwatar.foundation.topbar;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.EnumSet;
+
 import org.apache.wicket.util.tester.TagTester;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Test;
@@ -10,15 +12,41 @@ import org.junit.Test;
 public class FoundationTopBarPanelTest {
 
 	@Test
-	public void test() {
+	public void testBasic() {
 		WicketTester tester = new WicketTester();
 		FoundationTopBarPanel topBarPanel = new FoundationTopBarPanel("id");
 		tester.startComponentInPage(topBarPanel);
-		tester.dumpPage();
 		TagTester topBarContainer = tester.getTagByWicketId("topBarContainer");
 		TagTester topBar = tester.getTagByWicketId("topBar");
 		assertEquals("top-bar", topBar.getAttribute("class"));
 		assertTrue(topBar.hasAttribute("data-topbar"));
 		assertTrue(topBar.getAttributeIs("role", "navigation"));
+	}
+
+	@Test
+	public void testFixed() {
+		WicketTester tester = new WicketTester();
+		TopBarOptions options = new TopBarOptions().setFixed(true);
+		FoundationTopBarPanel topBarPanel = new FoundationTopBarPanel("id", options);
+		tester.startComponentInPage(topBarPanel);
+		TagTester topBarContainer = tester.getTagByWicketId("topBarContainer");
+		assertTrue(topBarContainer.getAttributeContains("class", "fixed"));
+		TagTester topBar = tester.getTagByWicketId("topBar");
+	}
+	
+	@Test
+	public void testPosition() {
+		WicketTester tester = new WicketTester();
+		TopBarOptions options = new TopBarOptions().setClickable(true).setContainToGrid(true)
+				.setSticky(true).setStickySizes(EnumSet.of(TopBarStickySize.LARGE));
+		FoundationTopBarPanel topBarPanel = new FoundationTopBarPanel("id", options);
+		tester.startComponentInPage(topBarPanel);
+		tester.dumpPage();
+		TagTester topBarContainer = tester.getTagByWicketId("topBarContainer");
+		assertTrue(topBarContainer.getAttributeContains("class", "contain-to-grid"));
+		assertTrue(topBarContainer.getAttributeContains("class", "sticky"));
+		TagTester topBar = tester.getTagByWicketId("topBar");
+		assertTrue(topBar.getAttributeContains("data-options", "is_hover:false"));
+		assertTrue(topBar.getAttributeContains("data-options", "sticky_on:large"));
 	}
 }
