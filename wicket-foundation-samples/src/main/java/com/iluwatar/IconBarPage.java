7d8f0edb94821710f6a15dd3ab630d9427442265
Ilkka Seppala <iluwatar@gmail.com>
2014-09-28 09:43:06 +0300

    Updated catalog page. Updated Foundation to 5.4.5. Added skeleton for icon bar sample page.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/IconBarPage.java
@@ -0,0 +1,13 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class IconBarPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public IconBarPage(PageParameters params) {
+		super(params);
+	}
+
+}
