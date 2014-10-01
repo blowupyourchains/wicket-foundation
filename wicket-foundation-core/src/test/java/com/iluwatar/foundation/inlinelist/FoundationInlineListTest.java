31532a3860258163842c88f36ae1604248879f1a
Ilkka Seppala <iluwatar@gmail.com>
2014-10-01 21:42:11 +0300

    Updated FoundationInlineList test case.

--- wicket-foundation-core/src/test/java/com/iluwatar/foundation/inlinelist/FoundationInlineListTest.java
+++ wicket-foundation-core/src/test/java/com/iluwatar/foundation/inlinelist/FoundationInlineListTest.java
@@ -1,11 +1,14 @@
 package com.iluwatar.foundation.inlinelist;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.link.AbstractLink;
+import org.apache.wicket.util.tester.TagTester;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Test;
 
@@ -23,6 +26,8 @@ public class FoundationInlineListTest {
 			public AbstractLink createLink(String id, int idx) {
 				return new AjaxLink<Void>(id) {
 
+					private static final long serialVersionUID = 1L;
+
 					@Override
 					public void onClick(AjaxRequestTarget target) {
 					}
@@ -33,6 +38,10 @@ public class FoundationInlineListTest {
 		};
 		tester.startComponentInPage(inlineList);
 		tester.dumpPage();
+		List<TagTester> textTags = tester.getTagsByWicketId("text");
+		assertEquals(3, textTags.size());
+		assertEquals("Foo", textTags.get(0).getValue());
+		assertEquals("Bar", textTags.get(1).getValue());
+		assertEquals("Baz", textTags.get(2).getValue());
 	}
-
 }
