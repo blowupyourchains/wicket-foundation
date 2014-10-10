509ff0586d4f1fd49f86ffb19d8dca5dc9261b77
Ilkka Seppala <iluwatar@gmail.com>
2014-10-10 22:17:02 +0300

    Added skeleton for progress bar sample page.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/ProgressBarPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class ProgressBarPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public ProgressBarPage(PageParameters params) {
+		super(params);
+	}
+}
