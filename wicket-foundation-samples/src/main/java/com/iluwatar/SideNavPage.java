3712be31337c25f6a41a3d330dd807deb6a7c2b1
Ilkka Seppala <iluwatar@gmail.com>
2014-10-11 14:36:20 +0300

    Added skeleton for side nav sample page.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/SideNavPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class SideNavPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public SideNavPage(PageParameters params) {
+		super(params);
+	}
+}
