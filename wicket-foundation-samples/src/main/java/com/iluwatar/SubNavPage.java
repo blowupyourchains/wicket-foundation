475d31bd49cf167d560f9cf0a252b379f82c6815
Ilkka Seppala <iluwatar@gmail.com>
2014-10-11 20:57:47 +0300

    Added skeleton for sub nav sample page.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/SubNavPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class SubNavPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public SubNavPage(PageParameters params) {
+		super(params);
+	}
+}
