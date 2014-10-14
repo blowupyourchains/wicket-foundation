5b7d34e8e3a286fe373652505715c148162127c4
Ilkka Seppala <iluwatar@gmail.com>
2014-10-14 21:55:18 +0300

    Added skeleton for split buttons sample page.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/SplitButtonsPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class SplitButtonsPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public SplitButtonsPage(PageParameters params) {
+		super(params);
+	}
+}
