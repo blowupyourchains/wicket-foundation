55a9622a7f42b90f0bcc786ff51bfe0fae2e7a06
Ilkka Seppala <iluwatar@gmail.com>
2015-02-17 21:23:42 +0200

    Added basic example for FoundationRevealModal component.

--- wicket-foundation-samples/src/main/java/com/iluwatar/RevealModalPage.java
+++ wicket-foundation-samples/src/main/java/com/iluwatar/RevealModalPage.java
@@ -1,6 +1,9 @@
 package com.iluwatar;
 
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.iluwatar.foundation.revealmodal.FoundationRevealModal;
 
 public class RevealModalPage extends BasePage {
 
@@ -8,5 +11,7 @@ public class RevealModalPage extends BasePage {
 
 	public RevealModalPage(PageParameters params) {
 		super(params);
+		final String content = "<h2>Awesome. I have it.</h2><p class=\"lead\">Your couch. It is mine.</p><p>I&#39;m a cool paragraph that lives inside of an even cooler modal. Wins!</p><a class=\"close-reveal-modal\">&#215;</a>";
+		add(new FoundationRevealModal("basic", Model.of("Click Me For A Modal"), Model.of(content)));
 	}
 }
