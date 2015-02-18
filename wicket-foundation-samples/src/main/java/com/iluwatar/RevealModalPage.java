a3ca56b32957d4bb0bb923a460c82321d8786ce8
Ilkka Seppala <iluwatar@gmail.com>
2015-02-18 21:35:16 +0200

    Added example reveal modal with video.

--- wicket-foundation-samples/src/main/java/com/iluwatar/RevealModalPage.java
+++ wicket-foundation-samples/src/main/java/com/iluwatar/RevealModalPage.java
@@ -13,5 +13,7 @@ public class RevealModalPage extends BasePage {
 		super(params);
 		final String content = "<h2>Awesome. I have it.</h2><p class=\"lead\">Your couch. It is mine.</p><p>I&#39;m a cool paragraph that lives inside of an even cooler modal. Wins!</p><a class=\"close-reveal-modal\">&#215;</a>";
 		add(new FoundationRevealModal("basic", Model.of("Click Me For A Modal"), Model.of(content)));
+		final String videoContent = "<h2>This modal has video</h2><div class=\"flex-video widescreen vimeo\"><iframe width=\"1280\" height=\"720\" src=\"//www.youtube-nocookie.com/embed/wnXCopXXblE?rel=0\" frameborder=\"0\" allowfullscreen></iframe></div><p><a class=\"close-reveal-modal\">&#215;</a>";
+		add(new FoundationRevealModal("video", Model.of("Example Modal with Video..."), Model.of(videoContent)));
 	}
 }
