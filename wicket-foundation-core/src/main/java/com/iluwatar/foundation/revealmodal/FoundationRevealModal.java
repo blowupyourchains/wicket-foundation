3102867d91fb7efa58aafa0ed77208f8af798421
Ilkka Seppala <iluwatar@gmail.com>
2015-02-18 20:46:00 +0200

    Added method for creating content panel in FoundationRevealModal. The method can be overridden by user to create custom content panels.

--- wicket-foundation-core/src/main/java/com/iluwatar/foundation/revealmodal/FoundationRevealModal.java
+++ wicket-foundation-core/src/main/java/com/iluwatar/foundation/revealmodal/FoundationRevealModal.java
@@ -1,7 +1,9 @@
 package com.iluwatar.foundation.revealmodal;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -11,21 +13,48 @@ import org.apache.wicket.model.IModel;
 import com.iluwatar.foundation.component.FoundationJsPanel;
 import com.iluwatar.foundation.util.Attribute;
 
+/**
+ * 
+ * Reveal modal component.
+ *
+ */
 public class FoundationRevealModal extends FoundationJsPanel {
 
 	private static final long serialVersionUID = 1L;
+	
+	private IModel<String> contentModel;
 
+	/**
+	 * Constructor with no content model. You should override {@link #createContentPanel(String)}
+	 * to create the content you need.
+	 * @param id wicket id
+	 * @param linkTextModel model for link text
+	 */
+	public FoundationRevealModal(String id, IModel<String> linkTextModel) {
+		this(id, linkTextModel, null);
+	}
+	
+	/**
+	 * Constructor with content model.
+	 * @param id wicket id
+	 * @param linkTextModel model for link text
+	 * @param contentModel model for html content
+	 */
 	public FoundationRevealModal(String id, IModel<String> linkTextModel, IModel<String> contentModel) {
 		super(id);
-		final RevealModalContent revealModalContent = new RevealModalContent("modalContent", contentModel);
-		add(revealModalContent);
+		this.contentModel = contentModel;
+		final WebMarkupContainer content = createContentPanel("modalContent");
+		content.add(new AttributeModifier("data-reveal", AttributeModifier.VALUELESS_ATTRIBUTE_ADD));
+		content.add(new AttributeAppender("class", "reveal-modal", " "));
+		add(content);
+		
 		AjaxLink<Void> openLink = new AjaxLink<Void>("openLink") {
 
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			protected void onComponentTag(ComponentTag tag) {
-				Attribute.addAttribute(tag, "data-reveal-id", revealModalContent.getMarkupId());
+				Attribute.addAttribute(tag, "data-reveal-id", content.getMarkupId());
 				super.onComponentTag(tag);
 			}
 			
@@ -36,7 +65,21 @@ public class FoundationRevealModal extends FoundationJsPanel {
 		add(openLink);
 		openLink.add(new Label("openLinkText", linkTextModel));
 	}
-	
+
+	/**
+	 * Creates content panel. Override this as necessary.
+	 * @param id
+	 * @return
+	 */
+	public WebMarkupContainer createContentPanel(String id) {
+		return new RevealModalContent(id, contentModel);
+	}
+
+	/**
+	 * 
+	 * Default content panel.
+	 *
+	 */
 	private static class RevealModalContent extends WebMarkupContainer {
 
 		private static final long serialVersionUID = 1L;
@@ -44,13 +87,6 @@ public class FoundationRevealModal extends FoundationJsPanel {
 		public RevealModalContent(String id, IModel<String> contentModel) {
 			super(id, contentModel);
 			this.setEscapeModelStrings(false);
-		}
-		
-		@Override
-		protected void onComponentTag(ComponentTag tag) {
-			Attribute.addClass(tag, "reveal-modal");
-			Attribute.addAttribute(tag, "data-reveal");
-			super.onComponentTag(tag);
 		}
 		
 		@Override
