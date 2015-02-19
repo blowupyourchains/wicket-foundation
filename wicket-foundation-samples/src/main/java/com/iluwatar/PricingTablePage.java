25d8b9767bea2ca637905328f347230013cbc94f
Ilkka Seppala <iluwatar@gmail.com>
2015-02-19 21:44:06 +0200

    Added skeleton for pricing table sample page.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/PricingTablePage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class PricingTablePage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public PricingTablePage(PageParameters params) {
+		super(params);
+	}
+}
