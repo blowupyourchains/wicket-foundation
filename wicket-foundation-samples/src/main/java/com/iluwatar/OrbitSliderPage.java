0e50ca77eb4b575971bc3261e36a0d67177ca6a9
Ilkka Seppala <iluwatar@gmail.com>
2015-02-21 21:18:26 +0200

    Setup skeleton for orbit slider sample page.

---
+++ wicket-foundation-samples/src/main/java/com/iluwatar/OrbitSliderPage.java
@@ -0,0 +1,12 @@
+package com.iluwatar;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class OrbitSliderPage extends BasePage {
+
+	private static final long serialVersionUID = 1L;
+
+	public OrbitSliderPage(PageParameters params) {
+		super(params);
+	}
+}
