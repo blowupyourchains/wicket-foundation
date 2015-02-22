8c20d98043ec8b6e7aa0644aaf4408055218875a
Ilkka Seppala <iluwatar@gmail.com>
2015-02-22 15:18:31 +0200

    Added FoundationOrbitContentSlider example.

--- wicket-foundation-samples/src/main/java/com/iluwatar/OrbitSliderPage.java
+++ wicket-foundation-samples/src/main/java/com/iluwatar/OrbitSliderPage.java
@@ -7,7 +7,9 @@ import org.apache.wicket.model.util.ListModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.PackageResourceReference;
 
+import com.iluwatar.foundation.orbitslider.FoundationOrbitContentSlider;
 import com.iluwatar.foundation.orbitslider.FoundationOrbitSlider;
+import com.iluwatar.foundation.orbitslider.OrbitSliderContent;
 import com.iluwatar.foundation.orbitslider.OrbitSliderItem;
 
 public class OrbitSliderPage extends BasePage {
@@ -22,5 +24,11 @@ public class OrbitSliderPage extends BasePage {
 		items.add(new OrbitSliderItem("3", new PackageResourceReference(this.getClass(), "satelite-orbit.jpg"), "Caption 3"));
 		FoundationOrbitSlider orbitSlider = new FoundationOrbitSlider("orbitSlider", new ListModel<>(items));
 		add(orbitSlider);
+		List<OrbitSliderContent> contents = new ArrayList<>();
+		contents.add(new OrbitSliderContent("1", "Heading 1", "Subheading 1"));
+		contents.add(new OrbitSliderContent("2", "Heading 2", "Subheading 2"));
+		contents.add(new OrbitSliderContent("3", "Heading 3", "Subheading 3"));
+		FoundationOrbitContentSlider contentSlider = new FoundationOrbitContentSlider("contentSlider", new ListModel<>(contents));
+		add(contentSlider);
 	}
 }
