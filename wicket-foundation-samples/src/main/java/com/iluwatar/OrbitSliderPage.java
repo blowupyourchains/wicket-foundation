652491f082c29c725775b63de55c9d6bc54d4544
Ilkka Seppala <iluwatar@gmail.com>
2015-02-22 15:02:18 +0200

    Added basic example for orbit slider.

--- wicket-foundation-samples/src/main/java/com/iluwatar/OrbitSliderPage.java
+++ wicket-foundation-samples/src/main/java/com/iluwatar/OrbitSliderPage.java
@@ -1,6 +1,14 @@
 package com.iluwatar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.model.util.ListModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.PackageResourceReference;
+
+import com.iluwatar.foundation.orbitslider.FoundationOrbitSlider;
+import com.iluwatar.foundation.orbitslider.OrbitSliderItem;
 
 public class OrbitSliderPage extends BasePage {
 
@@ -8,5 +16,11 @@ public class OrbitSliderPage extends BasePage {
 
 	public OrbitSliderPage(PageParameters params) {
 		super(params);
+		List<OrbitSliderItem> items = new ArrayList<>();
+		items.add(new OrbitSliderItem("1", new PackageResourceReference(this.getClass(), "andromeda-orbit.jpg"), "Caption 1"));
+		items.add(new OrbitSliderItem("2", new PackageResourceReference(this.getClass(), "launch-orbit.jpg"), "Caption 2"));
+		items.add(new OrbitSliderItem("3", new PackageResourceReference(this.getClass(), "satelite-orbit.jpg"), "Caption 3"));
+		FoundationOrbitSlider orbitSlider = new FoundationOrbitSlider("orbitSlider", new ListModel<>(items));
+		add(orbitSlider);
 	}
 }
